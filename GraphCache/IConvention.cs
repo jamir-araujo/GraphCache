66427d1cfc1d3db40357f1b78fd7ce30dd0d77e1
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2017-11-02 09:44:20 -0200

    Adding Dependency class and IConvention interface

---
+++ GraphCache/IConvention.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphCache.Core
+{
+    public interface IConvention<T>
+    {
+        bool FitsConvetion(T value);
+        string CreateKey(T value);
+    }
+}
