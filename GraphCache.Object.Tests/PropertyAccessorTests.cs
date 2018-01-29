10d0d561f26b4357ea1050a2af953943ec6bdcc5
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2018-01-29 18:16:56 -0200

    adding PropertyAccessor tests

---
+++ GraphCache.Object.Tests/PropertyAccessorTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace GraphCache.Object.Tests
+{
+    public class PropertyAccessorTests
+    {
+        private readonly Type _dataType;
+        private readonly PropertyInfo _propertyInfo;
+        private readonly PropertyAccessor<Data, int> _defaultAccessor;
+
+        public PropertyAccessorTests()
+        {
+            _dataType = typeof(Data);
+            _propertyInfo = _dataType.GetProperty("Value");
+
+            _defaultAccessor = new PropertyAccessor<Data, int>(_propertyInfo, data => data.Value, (data, value) => data.Value = value);
+        }
+
+        [Fact]
+        public void Constructor_Should_Throw_When_PropertyInfoIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("propertyInfo", () => new PropertyAccessor<Data, int>(null, (a) => 0, (a, b) => { }));
+        }
+
+        [Fact]
+        public void Constructor_Should_Throw_When_GetterIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("getter", () => new PropertyAccessor<Data, int>(_propertyInfo, null, (a, b) => { }));
+        }
+
+        [Fact]
+        public void Constructor_Should_Throw_When_SetterIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("setter", () => new PropertyAccessor<Data, int>(_propertyInfo, a => 0, null));
+        }
+
+        [Fact]
+        public void GetValue_Should_Throw_When_OwnerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("owner", () => _defaultAccessor.GetValue(null));
+        }
+
+        [Fact]
+        public void GetValue_Should_ReturnTheCorrectValue()
+        {
+            var data = new Data
+            {
+                Value = 1
+            };
+
+            var value = (int)_defaultAccessor.GetValue(data);
+
+            Assert.Equal(data.Value, value);
+        }
+
+        [Fact]
+        public void SetValue_Should_Throw_When_OwnerIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("owner", () => _defaultAccessor.SetValue(null, 0));
+        }
+
+        [Fact]
+        public void SetValue_Should_Throw_When_ValueIsNull()
+        {
+            Assert.Throws<ArgumentNullException>("value", () => _defaultAccessor.SetValue(new Data(), null));
+        }
+
+        [Fact]
+        public void SetValue_Should_SetTheValueProperty()
+        {
+            var data = new Data();
+
+            _defaultAccessor.SetValue(data, 30);
+
+            Assert.Equal(30, data.Value);
+        }
+
+        class Data
+        {
+            public int Value { get; set; }
+        }
+    }
+}
--- GraphCache.Object/PropertyAccessor.cs
+++ GraphCache.Object/PropertyAccessor.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using static GraphCache.Check;
 
 namespace GraphCache.Object
 {
@@ -6,7 +8,45 @@ namespace GraphCache.Object
     {
         public PropertyInfo PropertyInfo { get; }
 
+        protected PropertyAccessor(PropertyInfo propertyInfo)
+        {
+            NotNull(propertyInfo, nameof(propertyInfo));
+
+            PropertyInfo = propertyInfo;
+        }
+
         public abstract object GetValue(object owner);
         public abstract void SetValue(object owner, object value);
+    }
+
+    public class PropertyAccessor<TOwner, TProperty> : PropertyAccessor
+    {
+        private Func<TOwner, TProperty> _getter;
+        private Action<TOwner, TProperty> _setter;
+
+        public PropertyAccessor(PropertyInfo propertyInfo, Func<TOwner, TProperty> getter, Action<TOwner, TProperty> setter)
+            : base(propertyInfo)
+        {
+            NotNull(getter, nameof(getter));
+            NotNull(setter, nameof(setter));
+
+            _getter = getter;
+            _setter = setter;
+        }
+
+        public override object GetValue(object owner)
+        {
+            NotNull(owner, nameof(owner));
+
+            return _getter((TOwner)owner);
+        }
+
+        public override void SetValue(object owner, object value)
+        {
+            NotNull(owner, nameof(owner));
+            NotNull(value, nameof(value));
+
+            _setter((TOwner)owner, (TProperty)value);
+        }
     }
 }
